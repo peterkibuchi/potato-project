fba78d42e73ae3ff070e5ed23650284415f1d56a
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-07-01 10:40:05 +0300

    fix: Update `getBaseUrl` function to use production url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,5 @@
+import { getBaseUrl } from "~/lib/utils";
+
 type SiteConfig = {
   name: string;
   url: string;
@@ -11,17 +13,15 @@ type SiteConfig = {
   };
 };
 
-const baseUrl = "https://potato-project-jkuat.vercel.app";
-
 export const siteConfig: SiteConfig = {
   name: "Potato Project",
-  url: baseUrl,
+  url: getBaseUrl(),
   description:
     "Climate-smart potato production and consumption for improved nutrition and better health in Kenya.",
   creator: "Peter Kibuchi",
   authors: [{ name: "Peter Kibuchi", url: "https://github.com/peterkibuchi" }],
   keywords: ["Potato Project"],
-  ogImage: `${baseUrl}/og.png`,
+  ogImage: `${getBaseUrl()}/og.png`,
   links: {
     github: "https://github.com/peterkibuchi/potato-project",
   },
