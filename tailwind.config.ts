16f412b7b0908b3c4859c7396146035fa73cde14
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-07-05 10:00:35 +0300

    chore: Add `@tailwindcss/forms` plugin for styling form inputs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,5 +74,9 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
 } satisfies Config;
